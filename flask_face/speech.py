da2e96e8bcfb2e5e1166cf2d4aedc1622e0893e9
Kevin Cheung <kevin.cheung.sh@gmail.com>
2020-11-21 17:22:22 -0500

    initial

--- flask_face/speech.py
+++ flask_face/speech.py
@@ -8,66 +8,68 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 # Initialize the recognizer  
 r = sr.Recognizer()  
   
-# Function to convert text to 
-# speech 
-def SpeakText(command): 
-      
-    # Initialize the engine 
-    engine = pyttsx3.init() 
-    engine.say(command)  
-    engine.runAndWait() 
-      
-def sentiment_analysis(sentiment_text):
 
-    score = SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
+class textRecorder(object):
+    # Function to convert text to 
+    # speech 
+    def SpeakText(command): 
+        
+        # Initialize the engine 
+        engine = pyttsx3.init() 
+        engine.say(command)  
+        engine.runAndWait() 
+        
+    def sentiment_analysis(sentiment_text):
 
-    neg = score['neg']
+        score = SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
 
-    pos = score['pos']
+        neg = score['neg']
 
-    if neg > pos:
-        print("Negative :(")
-    elif pos > neg:
-        print("Positive :)")
-    else:
-        print("Neutral")
-      
+        pos = score['pos']
 
-      
-# Loop infinitely for user to 
-# speak 
-  
-while(1):     
-      
-    # Exception handling to handle 
-    # exceptions at the runtime 
-    try: 
-          
-        # use the microphone as source for input. 
-        with sr.Microphone() as source2: 
-              
-            # wait for a second to let the recognizer 
-            # adjust the energy threshold based on 
-            # the surrounding noise level  
-            r.adjust_for_ambient_noise(source2, duration=0.2) 
-              
-            print("Please speak")  
-            #listens for the user's input  
-            audio2 = r.listen(source2) 
-              
-            # Using ggogle to recognize audio 
-            MyText = r.recognize_google(audio2) 
-            MyText = MyText.lower() 
-  
-            print(MyText)
-            sentiment_analysis(MyText) 
-            SpeakText(MyText)
+        if neg > pos:
+            print("Negative :(")
+        elif pos > neg:
+            print("Positive :)")
+        else:
+            print("Neutral")
+        
+
+        
+    # Loop infinitely for user to 
+    # speak 
+    
+    while(1):     
+        
+        # Exception handling to handle 
+        # exceptions at the runtime 
+        try: 
+            
+            # use the microphone as source for input. 
+            with sr.Microphone() as source2: 
+                
+                # wait for a second to let the recognizer 
+                # adjust the energy threshold based on 
+                # the surrounding noise level  
+                r.adjust_for_ambient_noise(source2, duration=0.2) 
+                
+                print("Please speak")  
+                #listens for the user's input  
+                audio2 = r.listen(source2) 
+                
+                # Using ggogle to recognize audio 
+                MyText = r.recognize_google(audio2) 
+                MyText = MyText.lower() 
+    
+                print(MyText)
+                sentiment_analysis(MyText) 
+                SpeakText(MyText)
+                break 
+
+        except sr.RequestError as e: 
+            print("Could not request results; {0}".format(e))
             break 
-
-    except sr.RequestError as e: 
-        print("Could not request results; {0}".format(e))
-        break 
-          
-    except sr.UnknownValueError: 
-        print("unknown error occured") 
-        break
+            
+        except sr.UnknownValueError: 
+            print("unknown error occured") 
+            break
