4d9a995718ccb84192396356afcf40ad820918bf
Kevin Cheung <kevin.cheung.sh@gmail.com>
2020-11-21 00:52:55 -0500

    improved stt

--- speech-to-text.py
+++ speech-to-text.py
@@ -1,16 +1,41 @@
+# import os
 import speech_recognition as sr
+import subprocess
+# import ffmpeg
+# from moviepy.editor import *
+
+import moviepy.editor as mp
+
+clip = mp.VideoFileClip("test.mp4").subclip(0,20)
+clip.audio.write_audiofile("test.mp3")
+
+
+# audioclip = AudioFileClip("test.mp4")
+# audioclip.write_audiofile("audio.wav")
+
+
+
+# command2mp3 = "ffmpeg -i test.mp4 test.mp3"
+
+# command2wav = "ffmpeg -i test.mp3 test.wav"
+
+# os.system(command2mp3)
+# os.system(command2wav)
+
 
 r = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print("Talk")
-    audio_text = r.listen(source)
-    print("Time over, thanks")
-
-    # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
+with sr.AudioFile('test.mp3') as source:
     
+    audio_text = r.listen(source)
+    
+# recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
     try:
+        
         # using google speech recognition
-        print("Text: "+r.recognize_google(audio_text))
+        text = r.recognize_google(audio_text)
+        print('Converting audio transcripts into text ...')
+        print(text)
+     
     except:
-         print("Sorry, I did not get that")
+         print('Sorry.. run again...')
